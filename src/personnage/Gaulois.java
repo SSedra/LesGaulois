a0bc558e4ef7d317037edcf1961ad0510baddfd3
SSedra <sahy-sedra.rakotoniaina@univ-tlse3.fr>
2025-02-13 12:04:00 +0100

    premier commit

---
+++ src/personnage/Gaulois.java
@@ -0,0 +1,24 @@
+package personnage;
+
+public class Gaulois {
+	public int force;
+	public String nom;
+	
+	public Gaulois(String nom , int force) {
+		this.nom=nom;
+		this.force=force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		System.out.println(prendreParole() +"\"" + texte + "\"");
+	}
+	
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+		}
+	
+}
